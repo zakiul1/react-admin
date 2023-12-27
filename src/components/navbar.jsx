0d492c36010d7410182d3357d328ef02efbd3e89
zakiul1 <86553317+zakiul1@users.noreply.github.com>
2023-12-27 23:51:03 +0600

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button, DarkThemeToggle, Navbar } from "flowbite-react";
+
+const ExampleNavbar = function () {
+  return (
+    <Navbar fluid>
+      <div className="w-full p-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Navbar.Brand href="/">
+              <img alt="" src="/images/logo.svg" className="mr-3 h-6 sm:h-8" />
+              <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                Flowbite
+              </span>
+            </Navbar.Brand>
+          </div>
+          <div className="flex items-center gap-3">
+            <DarkThemeToggle />
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  );
+};
+
+export default ExampleNavbar;
